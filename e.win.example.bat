53f87ab7ff93ebbb8e17f9c0b049aec59a0f2ea2
truetamtam <truetamtam@yandex.ru>
2015-09-08 18:24:52 +0300

    Added /wait option to e.win.example.bat

--- e.win.example.bat
+++ e.win.example.bat
@@ -2,4 +2,4 @@ REM Edit for your machine
 REM Tested on windows7 with msysgit
 d:
 cd d:\path\to\cloned\repository
-start /min "" "c:\Program Files\Git\bin\bash.exe" ./e.sh
+start /wait /min "" "c:\Program Files\Git\bin\bash.exe" ./e.sh
