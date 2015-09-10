5fc23513293c041a697b1b950e8d0cc47d77b905
truetamtam <truetamtam@yandex.ru>
2015-09-10 14:43:15 +0300

    d.sh ok.

--- d.sh
+++ d.sh
@@ -7,70 +7,104 @@
 # 3. Decrypt key with private key and password prompt
 # 4. Decrypt archive with decrypted key
 # 5. Delete decrypted key
-PATH_TO_ARCH=""; FILE_NAME=""; PATH_TO_OUT=""; PATH_TO_KEY=""; PATH_TO_PEM=""
 
 
-# enc'ted file location
-while [[ ! -f "$PATH_TO_ARCH" ]]; do
-    if [[ ! -f "$PATH_TO_ARCH" ]] && [[ ! -z "$PATH_TO_ARCH" ]]; then echo "$PATH_TO_ARCH is not a file."; fi
-    read -e -p "Absolute path to archive: " PATH_TO_ARCH
-done
-echo "---"
-
-# stripping file name
-FILE_NAME_ENC=$(basename ${PATH_TO_ARCH})
-FILE_NAME=${FILE_NAME_ENC%.enc}
+PATH_TO_ARCH=""; FILE_NAME=""; PATH_TO_OUT=""; PATH_TO_KEY=""; PATH_TO_PEM=""; ANSWER="n"
 
 
-# path to output decrypted file
-while [[ -z "$PATH_TO_OUT" ]] || [[ -f ${PATH_TO_OUT} ]]; do
-    if [[ -f ${PATH_TO_OUT} ]]; then echo "File: $PATH_TO_OUT already exists."; fi
-    read -e -p "Output file path: " PATH_TO_OUT
-done
-echo "---"
+# reset vars
+function cleanup() {
+    PATH_TO_ARCH=""; FILE_NAME=""; PATH_TO_OUT=""; PATH_TO_KEY=""; PATH_TO_PEM=""; ANSWER="n"
+}
 
 
-# enc'ted key location
-while [[ -z ${PATH_TO_KEY} ]] || [[ ! -f ${PATH_TO_KEY} ]]; do
-    if [[ ! -f ${PATH_TO_KEY} ]]; then echo "$PATH_TO_KEY is not a file."; fi
-    read -e -p "Absolute path to enc'ted key: " PATH_TO_KEY
-done
-echo "---"
+# set up all vars
+function setup() {
+    # enc'ted file location
+    while [[ ! -f "$PATH_TO_ARCH" ]]; do
+        if [[ ! -f "$PATH_TO_ARCH" ]] && [[ ! -z "$PATH_TO_ARCH" ]]; then echo "$PATH_TO_ARCH is not a file."; fi
+        read -e -p "Absolute path to archive: " PATH_TO_ARCH
+    done
+    echo "---"
+
+    # stripping file name
+    FILE_NAME_ENC=$(basename ${PATH_TO_ARCH})
+    FILE_NAME=${FILE_NAME_ENC%.enc}
 
 
-# private key location
-while [[ ! -f ${PATH_TO_PEM} ]]; do
-    if [[ ! -f ${PATH_TO_PEM} ]]; then echo "$PATH_TO_PEM is not a file."; fi
-    read -e -p "Absolute path to pem: [private.pem] " PATH_TO_PEM
-done
+    # path to output decrypted file
+    while [[ -z "$PATH_TO_OUT" ]] || [[ -f ${PATH_TO_OUT} ]]; do
+        read -e -p "Output file path: " PATH_TO_OUT
+        if [[ -f ${PATH_TO_OUT} ]]; then echo "File: $PATH_TO_OUT already exists."; fi
+    done
+    echo "---"
 
 
-echo "Private key absoulute path. Default:[./private.pem]"
-
-read PATH_TO_PEM
-
-if [[ "$PATH_TO_PEM" == "" ]]; then PATH_TO_PEM=./private.pem; fi
-
-if [[ ! -f ${PATH_TO_PEM} ]]; then echo "Private key is not a file. Exiting..."; exit 1; fi
-
-echo "---"
-echo "=================================="
-printf "File path is:\t%s\n" ${PATH_TO_ARCH}
-printf "Enc file name is:\t%s\n" ${FILE_NAME_ENC}
-printf "File name is:\t%s\n" ${FILE_NAME}
-echo "=================================="
-
-# decrypting key with private.pem
-echo "Decrypting aes key with rsa private key..."
-openssl rsautl -decrypt -inkey ${PATH_TO_PEM} -in ${PATH_TO_KEY} -out ${FILE_NAME}.key
+    # enc'ted key location
+    while [[ -z ${PATH_TO_KEY} ]] || [[ ! -f ${PATH_TO_KEY} ]]; do
+        read -e -p "Absolute path to enc'ted key: " PATH_TO_KEY
+        if [[ ! -f ${PATH_TO_KEY} ]]; then echo "$PATH_TO_KEY is not a file."; fi
+    done
+    echo "---"
 
 
-# decrypting file with decrypted aes key
-echo "Decrypting file with aes key..."
-openssl enc -d -aes-256-cbc -in ${PATH_TO_ARCH} -out ./${PATH_TO_OUT} -pass file:${FILE_NAME}.key
+    # private key location
+    while [[ -z ${PATH_TO_PEM} ]] || [[ ! -f ${PATH_TO_PEM} ]]; do
+        read -e -p "Absolute path to pem: [./private.pem] " PATH_TO_PEM
+        # if read is "" than it means user wants default
+        if [[ -z ${PATH_TO_PEM} ]]; then PATH_TO_PEM=${PATH_TO_PEM:=./private.pem}; fi
+        if [[ ! -f ${PATH_TO_PEM} ]]; then echo "$PATH_TO_PEM is not a file."; fi
+    done
+    echo "---"
 
 
-# removing decrypted aes key
-echo "Removing decrypted aes key..."
-rm -f ./${FILE_NAME}.key
+    echo "=================================="
+    printf "File path is:\t\t%s\n" ${PATH_TO_ARCH}
+    printf "Enc'ted file name:\t%s\n" ${FILE_NAME_ENC}
+    printf "File name is:\t\t%s\n" ${FILE_NAME}
+    printf "Output file is:\t\t%s\n" ${PATH_TO_OUT}
+    printf "Enc'ted key:\t\t%s\n" ${PATH_TO_KEY}
+    printf "Key pem:\t\t%s\n" ${PATH_TO_PEM}
+    echo "=================================="
+}
+
+
+# decrypt
+function decrypt() {
+    # decrypting key with private.pem
+    echo "Decrypting aes key with rsa private key..."
+    openssl rsautl -decrypt -inkey ${PATH_TO_PEM} -in ${PATH_TO_KEY} -out ${FILE_NAME}.key
+
+    # chcking that the key was decrypted
+    if [[ ! -f ${FILE_NAME}.key ]]; then echo "Error decrypting rsa key."; exit 1; fi
+
+    # decrypting file with decrypted aes key
+    echo "Decrypting file with aes key..."
+    openssl enc -d -aes-256-cbc -in ${FILE_NAME_ENC} -out ${PATH_TO_OUT} -pass file:${FILE_NAME}.key
+
+    # removing decrypted aes key
+    echo "Removing decrypted aes key..."
+    rm -f ${FILE_NAME}.key
+}
+
+
+# main
+function run() {
+    setup
+    while [ "$ANSWER" != "y" ]; do
+        read -p "Setup ok?[$ANSWER]" ANSWER
+        ANSWER=${ANSWER:-n}
+        if [ "${ANSWER}" = "y" ]; then
+            decrypt
+            echo "Decryption successful."
+        else
+            cleanup
+            setup
+        fi
+    done
+}
+
+
+# start program
+run
 
